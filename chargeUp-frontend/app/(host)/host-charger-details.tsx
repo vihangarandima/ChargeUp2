a875170d1ec4d947661769968ee44d98ee22a62e
AshanMalidu <ashan.20230392@iit.ac.lk>
2026-03-12 22:47:09 +0530

    host charger details

---
+++ chargeUp-frontend/app/(host)/host-charger-details.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  StyleSheet,
+  StatusBar,
+  ScrollView,
+  Alert,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
+import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
+
+export default function HostChargerDetails() {
+  const router = useRouter();
