63c60987519ca2d1c4b91e12f268499802d43c61
AshanMalidu <ashan.20230392@iit.ac.lk>
2026-03-13 20:29:17 +0530

    add  Automatically find user location on component mount

--- chargeUp-frontend/app/(host)/location-picker.tsx
+++ chargeUp-frontend/app/(host)/location-picker.tsx
@@ -14,3 +14,20 @@ import { useRouter } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import * as Location from "expo-location";
+
+export default function LocationPicker() {
+  const router = useRouter();
+  const mapRef = useRef(null);
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedLocation, setSelectedLocation] = useState({
+    latitude: 6.9271,
+    longitude: 79.8612,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.05,
+  });
+
+  // Automatically find user location on component mount
+  useEffect(() => {
+    getUserLocation();
+  }, []);
